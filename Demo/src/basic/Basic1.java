e8d5050f330ae1687a2eae9457941f5b81b6cdcc
Kavya Rajendran <Kavya Rajendran@Dell-E7490>
2026-09-25 20:18:54 +0530

    Basic

---
+++ Demo/src/basic/Basic1.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class Basic1 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println(a);
+		System.out.println(b);
+		int res = a+b;
+		System.out.println(res);
+	}
+
+}
